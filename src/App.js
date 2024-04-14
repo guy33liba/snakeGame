c58e2ac386ae4779c4c51809ad08bd3f8cd8dd97
guy liba <guy33liba@gmail.com>
2024-04-14 09:09:03 +0300

    my first snake game

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Snake from "./snake/Snake"
+import "./App.css"
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+      <Snake />
+    </div>
+  )
+}
+
+export default App
